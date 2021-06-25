8dbefd6dade1c8aa2f8526e5f0b60d78d7255544
dima_usac <usachev2012@gmail.com>
2021-06-25 13:26:02 +0400

    Initial commit

---
+++ siteblog/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', Home.as_view(), name='home'),
+    path('category/<str:slug>/', PostsByCategory.as_view(), name='category'),
+    path('post/<str:slug>/', get_post, name='post'),
+
+]
